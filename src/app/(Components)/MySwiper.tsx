92a4ad41c1f0552aad00ae74c0f6e2489907976a
raymasahil <bhoomi@teque7.com>
2024-05-08 15:12:57 +0530

    change banner text

--- src/app/(Components)/MySwiper.tsx
+++ src/app/(Components)/MySwiper.tsx
@@ -44,7 +44,12 @@ const MySwiper: React.FC = () => {
 
                   <div className=''>
 
-                    <h1 className='w-100 text-6xl font-bold text-pink-500 pt-40 '>Making <br></br>Womanhood<br></br> Wonderful.....</h1> 
+                  <h1 className='w-100 text-6xl font-bold text-pink-500 pt-40'>
+                    <span style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', lineHeight: '1.5' }}>Making</span> <br />
+                    <span style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', lineHeight: '1.5' }}>Womanhood</span><br />
+                    <span style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', lineHeight: '1.5' }}>Wonderful.....</span>
+                  </h1>
+
                     {/* <p className='font-medium text-pink-500'>Promoting good health together.</p>  */}
                     {/* <button className="border border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white ransition-all duration-300 mt-3 pt-2 pb-2 rounded-3xl pl-4 pr-4 shadow-lg flex items-center">
                       Button <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-2">
@@ -66,12 +71,12 @@ const MySwiper: React.FC = () => {
                     </div>
                   </div>
                   
-                  <div className='flex items-center gap-4 absolute mt-28'>
+                  <div className='flex items-center gap-4 absolute mt-20'>
                       <div>
                         <Image src="/Images/Safety.png" width={110} height={110} alt="Safety" />
                       </div>
-                      <div className='w-96 text-slate-500'>
-                        <p>National Accreditation Board for Hospitals & Healthcare Providers (NABH)</p>
+                      <div className='w-96 text-slate-600'>
+                        <p>National Accreditation Board for Hospitals <br></br>& Healthcare Providers (NABH)</p>
                       </div>
                   </div>
 
@@ -83,8 +88,14 @@ const MySwiper: React.FC = () => {
           <SwiperSlide className="flex w-full justify-center items-center bg-[url('/Images/Main-Banner-Website-Banner.jpg')] bg-cover bg-no-repeat"> 
             <div className='max-w-[1280px] mx-auto w-full h-full px-4 flex flex-row items-center'>
             <div className='w-full md:w-1/2 animate-fade-in'>
-                  <h1 className='w-[600px] text-5xl font-bold pb-4 text-slate-600 pt-44 mr-4'>We are committed to your health and well-being</h1> 
-                  <p className='font-medium w-96 text-pink-500'>We are dedicated to providing comprehensive healthcare services tailored specifically to address the unique needs of women.</p> 
+            <h1 className='w-[600px] text-6xl font-bold pb-4 text-slate-600 pt-44'>
+              <span style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', lineHeight: '1.5' }}>We are committed </span><br />
+              <span style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', lineHeight: '1.5' }}> to  your health</span> <br />
+              <span style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', lineHeight: '1.5' }}>and well-being</span>
+            </h1>
+
+ 
+                  {/* <p className='font-medium w-96 text-pink-500'>We are dedicated to providing comprehensive healthcare services tailored specifically to address the unique needs of women.</p>  */}
                   <div className="navigation-buttons p-20 ">
                           <div className="swiper-button-prev">
                               <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
